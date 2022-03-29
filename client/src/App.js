4efeb808a116290f187fa89f3c90538a2ce7d504
MartinezBruno <brunomar006@gmail.com>
2022-03-28 23:27:40 -0300

    rotues created

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,17 @@
-import './App.css';
+import './App.css'
+import {Route, Switch} from 'react-router-dom'
+import LandingPage from './components/LandingPage/LandingPage.jsx'
+import Home from './components/Home/Home.jsx'
 
 function App() {
-  return (
-    <div className="App">
-      <h1>Henry Videogames</h1>
-    </div>
-  );
+   return (
+      <div className="App">
+         <Switch>
+            <Route exact path="/" component={LandingPage} />
+            <Route path="/home" component={Home} />
+         </Switch>
+      </div>
+   )
 }
 
-export default App;
+export default App
