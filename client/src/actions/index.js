8246c47d168a5706efa7d220631300fbded234b6
MartinezBruno <brunomar006@gmail.com>
2022-03-29 10:40:39 -0300

    getGenders and filterByGenre created

--- client/src/actions/index.js
+++ client/src/actions/index.js
@@ -2,15 +2,24 @@ import axios from 'axios'
 
 export const getVideogames = () => async dispatch => {
    var json = await axios.get('/videogames')
-   console.log(json)
+   // console.log(json, 'videogames')
    return dispatch({
       type: 'GET_VIDEOGAMES',
       payload: json.data,
    })
 }
+export const getGenres = () => async dispatch => {
+   var json = await axios.get('/genres')
+   console.log(json.data, 'genres')
+   return dispatch({
+      type: 'GET_GENRES',
+      payload: json.data,
+   })
+}
+
 export const filterByApiOrDb = payload => async dispatch => {
    try {
-      console.log(payload, 'filterByDbOrApi ACTION')
+      // console.log(payload, 'filterByDbOrApi ACTION')
       dispatch({
          type: 'FILTER_BY_API_OR_DB',
          payload,
@@ -24,6 +33,17 @@ export const order = payload => async dispatch => {
    try {
       dispatch({
          type: 'ORDER',
+         payload,
+      })
+   } catch (error) {
+      console.log(error)
+   }
+}
+
+export const filterByGenre = payload => async dispatch => {
+   try {
+      dispatch({
+         type: 'FILTER_BY_GENRE',
          payload,
       })
    } catch (error) {
