1b3b1dbcefa20317a87e317e13f5922f3c0e6495
MartinezBruno <brunomar006@gmail.com>
2022-03-30 20:52:19 -0300

    loading deleted, searchByName added, postVideoGame added

--- client/src/reducer/index.js
+++ client/src/reducer/index.js
@@ -1,12 +1,9 @@
 const initialState = {
    videogames: [],
    allVideogames: [],
-   genres: [],
    videogameDetail: [],
-   loading: {
-      loading: false,
-      message: '',
-   },
+   videogameSearched: [],
+   genres: [],
 }
 
 function rootReducer(state = initialState, {type, payload}) {
@@ -17,10 +14,12 @@ function rootReducer(state = initialState, {type, payload}) {
             videogames: payload,
             allVideogames: payload,
             videogameDetail: [],
-            loading: {
-               loading: false,
-               msg: '',
-            },
+            videogameSearched: [],
+         }
+      case 'SEARCH_VIDEOGAME':
+         return {
+            ...state,
+            videogameSearched: payload,
          }
       case 'GET_VIDEOGAME_DETAILS':
          return {
@@ -28,30 +27,32 @@ function rootReducer(state = initialState, {type, payload}) {
             videogameDetail: payload,
             videogames: [],
             allVideogames: [],
-            loading: {
-               loading: false,
-               msg: '',
-            },
          }
+      case 'POST_VIDEOGAME':
+         return {
+            ...state,
+            allVideogames: payload,
+         }
+
       case 'GET_GENRES':
          return {
             ...state,
             genres: payload,
             loading: {
                loading: false,
-               msg: '',
+               message: '',
             },
          }
-      case 'FILTER_BY_API_OR_DB': 
-         const allVideogames = state.allVideogames 
+      case 'FILTER_BY_API_OR_DB':
+         const allVideogames = state.allVideogames
          const filteredVideogames =
             payload === 'created'
                ? allVideogames.filter(game => game.createdOnDb)
-               : allVideogames.filter(game => typeof game.id === 'number') 
+               : allVideogames.filter(game => typeof game.id === 'number')
          return {
-            ...state, 
+            ...state,
             videogames:
-               payload === 'all' ? state.allVideogames : filteredVideogames, 
+               payload === 'all' ? state.allVideogames : filteredVideogames,
          }
       case 'ORDER':
          let orderedGames
@@ -71,16 +72,12 @@ function rootReducer(state = initialState, {type, payload}) {
          }
          if (payload === 'descR') {
             orderedGames = state.videogames.sort((a, b) => {
-               if (a.rating < b.rating) return 1
-               if (a.rating > b.rating) return -1
-               return 0
+               return a.rating - b.rating
             })
          }
          if (payload === 'ascR') {
             orderedGames = state.videogames.sort((a, b) => {
-               if (a.rating < b.rating) return -1
-               if (a.rating > b.rating) return 1
-               return 0
+               return b.rating - a.rating
             })
          }
          return {
@@ -101,14 +98,6 @@ function rootReducer(state = initialState, {type, payload}) {
          return {
             ...state,
             videogames: filteredGames,
-         }
-      case 'LOADING_VIDEOGAMES':
-         return {
-            ...state,
-            loading: {
-               loading: true,
-               message: payload,
-            },
          }
       default:
          return state
